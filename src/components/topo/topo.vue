39f954d44a8e78157ccf114cd403b51aa5d06e32
jim <473723204@qq.com>
2021-09-06 15:09:43 +0800

    123

---
+++ src/components/topo/topo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="topo" ref="cutTopo">
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name:'topo',
+    beforeCreate(){
+      // console.log('beforeCreaterecommedn');
+    },
+    created() {
+      console.log('recommedn');
+      
+    },
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  
+</style>
