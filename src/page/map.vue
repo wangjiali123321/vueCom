826fef3b7eb027f63d73cb5be6b47a78b65c16a2
jim <473723204@qq.com>
2021-09-27 19:56:54 +0800

    23

--- src/page/map.vue
+++ src/page/map.vue
@@ -8,6 +8,7 @@
 <script type="text/ecmascript-6">
 import echarts from 'echarts' // echarts支持
 import 'echarts/map/js/china' // 中国地图js
+
 export default {
 components: { jimTable },
   name:'recommend',
@@ -47,6 +48,52 @@ components: { jimTable },
   },
   mounted() {
     this.chainMap()
+  },
+  methods() {
+    chainMap () {
+      let myChart = echarts.init(document.getElementById('chainMap'));
+      let option = {
+        tooltip: {
+          show: false
+        },
+        geo: {
+          map: 'china',
+          roam: false,// 一定要关闭拖拽
+          zoom: 1.23,
+          center: [105, 36], // 调整地图位置
+          label: {
+            normal: {
+              show: true, //关闭省份名展示
+              fontSize: "12",
+              color: "rgba(0,0,0,1)"
+            },
+            emphasis: {
+              show: false
+            }
+          },
+          itemStyle: {
+            normal: {
+              areaColor: "#2707e3",
+              borderColor: "#1cdc17",
+              borderWidth: 1, //设置外层边框
+              shadowBlur: 5,
+              shadowOffsetY: 8,
+              shadowOffsetX: 0,
+              shadowColor: "#133d18"
+            },
+            emphasis: {
+              areaColor: "#bd8f35",
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowBlur: 5,
+              borderWidth: 0,
+              shadowColor: "rgba(104,83,22,0.5)"
+            }
+          }
+        }
+      };
+      myChart.setOption(option);
+    }
   }
 }
 </script>
