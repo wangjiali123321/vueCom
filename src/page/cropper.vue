4b951db6c876ed17abf1afefc757c6a38851fdd0
jim <473723204@qq.com>
2022-05-24 17:03:00 +0800

    1

--- src/page/cropper.vue
+++ src/page/cropper.vue
@@ -7,7 +7,13 @@
     <input type="file"
       ref="fileInput"
       @change="_handleInputChange"/>
-    <canvas ref="canvas"></canvas>
+    <canvas ref="canvas"
+      @touchstart.stop="_handlePointerStart"
+      @mousedown.stop.prevent="_handlePointerStart"
+      @pointerstart.stop.prevent="_handlePointerStart"        
+      @touchmove.stop="_handlePointerMove"
+      @mousemove.stop.prevent="_handlePointerMove"
+      @pointermove.stop.prevent="_handlePointerMove"></canvas>
   </div>
 </template>
 
@@ -52,41 +58,75 @@ export default {
       }
       fr.readAsDataURL(file)
     },
-    _drawFrame(img){
-      this.canvas = this.$refs.canvas
-      this.canvas.width = 500
-      this.canvas.height = 500
-      this.canvas.style.width = '400px'
-      this.canvas.style.height = '400px'
+    // _drawFrame(img){
+    //   this.canvas = this.$refs.canvas
+    //   this.canvas.width = 500
+    //   this.canvas.height = 500
+    //   this.canvas.style.width = '400px'
+    //   this.canvas.style.height = '400px'
 
-      this.ctx = this.canvas.getContext('2d')
-      this.ctx.imageSmoothingEnabled = true;
-      this.ctx.imageSmoothingQuality = "high";
-      this.ctx.webkitImageSmoothingEnabled = true;
-      this.ctx.msImageSmoothingEnabled = true;
-      this.ctx.imageSmoothingEnabled = true;
+    //   this.ctx = this.canvas.getContext('2d')
+    //   this.ctx.imageSmoothingEnabled = true;
+    //   this.ctx.imageSmoothingQuality = "high";
+    //   this.ctx.webkitImageSmoothingEnabled = true;
+    //   this.ctx.msImageSmoothingEnabled = true;
+    //   this.ctx.imageSmoothingEnabled = true;
 
-      // this.ctx.fillStyle = 'yellow'
-      // this.outputWidth = 6000
-      // this.outputHeight = 6000
-      // this.ctx.clearRect(0, 0, this.outputWidth, this.outputHeight)
-      // this.ctx.fillRect(0, 0, this.outputWidth, this.outputHeight)
+    //   // this.ctx.fillStyle = 'yellow'
+    //   // this.outputWidth = 6000
+    //   // this.outputHeight = 6000
+    //   // this.ctx.clearRect(0, 0, this.outputWidth, this.outputHeight)
+    //   // this.ctx.fillRect(0, 0, this.outputWidth, this.outputHeight)
 
-      this.imgData = {
-        startX: 0,
-        startY: 0,
-        width: 800,
-        height: 800
+    //   this.imgData = {
+    //     startX: 0,
+    //     startY: 0,
+    //     width: 800,
+    //     height: 800
+    //   }
+
+    //   let { startX , startY, width, height} = this.imgData
+    //   console.log( startX, startY, width, height)
+    //   console.log('this.img',img)
+    //   this.ctx.drawImage(img, startX, startY, width, height)
+    //   this.canvas.toBlob((e)=>{
+    //     console.log('e',e)
+    //   })
+    // },
+    
+    move (offset) {
+      let oldX = this.imgData.startX
+      let oldY = this.imgData.startY
+      this.imgData.startX += offset.x
+      this.imgData.startY += offset.y
+      if (this.imgData.startX !== oldX || this.imgData.startY !== oldY) {
+        this._draw()
       }
+    },
+    _handlePointerStart (){
 
-      let { startX , startY, width, height} = this.imgData
-      console.log( startX, startY, width, height)
-      console.log('this.img',img)
-      this.ctx.drawImage(img, startX, startY, width, height)
-      this.canvas.toBlob((e)=>{
-        console.log('e',e)
+    },
+    _handlePointerMove () {
+
+    },
+    _draw () {
+      this.$nextTick(() => {
+        if (typeof window !== 'undefined' && window.requestAnimationFrame) {
+          requestAnimationFrame(this._drawFrame)
+        } else {
+          this._drawFrame()
+        }
       })
-    }
+    },
+
+    _drawFrame () {
+      let ctx = this.ctx
+      let { startX, startY, width, height } = this.imgData
+      console.log( startX, startY, width, height)
+      this._paintBackground()
+      // console.log('this.img',this.img)
+      ctx.drawImage(this.img, startX, startY, width, height)
+    },
   }
 }
 </script>
